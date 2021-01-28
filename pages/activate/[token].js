1993c749246bfa61f096282e3331c5462d2c8d72
Dominik Guzy <dominik.guzy@gorrion.pl>
2021-01-28 10:43:51 +0100

    next

--- pages/activate/[token].js
+++ pages/activate/[token].js
@@ -4,8 +4,13 @@ export function getStaticPaths() {
   return { paths: [], fallback: true };
 }
 
-export default function Activate() {
-  const router = useRouter();
-  const { token } = router.query;
+export function getStaticProps({ params }) {
+  return {
+    props: { token: params.token },
+    unstable_revalidate: true,
+  };
+}
+
+export default function Activate({ token }) {
   return <h1>{token}</h1>;
 }
